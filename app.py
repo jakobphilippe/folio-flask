83b1144206687549ac81ffb22bb1db7053087c6f
Jakob Philippe <philippe.j@northeastern.edu>
2021-08-17 13:28:46 +0200

    cors update

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, jsonify
 import yfinance as yf
 from multiprocessing.dummy import Pool as ThreadPool
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return 'Hello World!'
 
 
 @app.route('/api/stock/card_data', methods=['POST'])
+@cross_origin()
 def stock_card_data():
     tickers = request.json['tickers']
     pool = ThreadPool(100)
