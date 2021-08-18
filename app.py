a42bf0d86bef97d6a581574181ed2d7f4f46ab17
Jakob Philippe <philippe.j@northeastern.edu>
2021-08-18 14:12:30 +0200

    added yflive for faster quote updates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import yfinance as yf
 from multiprocessing.dummy import Pool as ThreadPool
 from flask_cors import CORS, cross_origin
+from yflive import QuoteStreamer
 
 app = Flask(__name__)
 CORS(app)
@@ -26,7 +27,6 @@ def set_card_data(ticker):
     data_dict = {}
     t = yf.Ticker(ticker)
     info = t.info
-    print(info)
     data_dict["ticker"] = ticker
     data_dict["name"] = info['shortName']
     if "sector" in info:
@@ -41,6 +41,35 @@ def set_card_data(ticker):
     data_dict["price_change"] = "%.2f" % (price - open_price)
 
     return data_dict
+
+
+@app.route('/api/stock/quick_quote', methods=['POST'])
+@cross_origin()
+def quick_quote():
+    tickers = request.json['tickers']
+    data = []
+    for stock in tickers:
+        getQuote([stock], data)
+
+    return jsonify(data)
+
+
+def getQuote(quote, data):
+    qs = QuoteStreamer()
+    qs.subscribe(quote)
+
+    qs.on_quote = lambda qs, q: handleOnQuote(q, qs, data)
+
+    qs.start(should_thread=False)
+
+    return data
+
+
+def handleOnQuote(q, qs, data):
+    qs.unsubscribe(q.identifier)
+    obj = {'ticker': q.identifier, 'price': "%.2f" % q.price, 'percent_change': "%.2f" % q.changePercent, 'price_change': "%.2f" % q.change}
+    data.append(obj)
+    qs.stop()
 
 
 if __name__ == '__main__':
